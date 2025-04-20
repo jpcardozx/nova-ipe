602894f81ebb171fc000871043155bcede4ef15f
jpcardozx <jpcardozoo0106@gmail.com>
2025-04-20 18:20:20 -0300

    sanity.config.ts import update

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk"; // correto
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./studio/schemas";
-import { structure } from "./sanity/customStructure"; // importa sua estrutura customizada
-import { projectId, dataset, apiVersion } from "./sanity/env";
+import { structure } from "./studio/sanity/customStructure"; // importa sua estrutura customizada
+import { projectId, dataset, apiVersion } from "./studio/sanity/env";
 
 export default defineConfig({
   basePath: "/studio",
