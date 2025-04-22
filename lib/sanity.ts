fb6877df7e7d784f6c7dc4aae25ab3b58af20559
jpcardozx <jpcardozoo0106@gmail.com>
2025-04-22 15:46:52 -0300

    next-sanity install

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from 'next-sanity'
 export const sanityClient = createClient({
   projectId: '0nks58lj',
   dataset: 'production',
-  apiVersion: '2024-04-01',
+  apiVersion: '2024-04-25',
   useCdn: false // true se for apenas leitura pública
 })
